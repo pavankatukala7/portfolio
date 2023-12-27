a902e360e89f3eeb04baeeea7e41d92a847e59fe
Pavan Katukala <pavan.katukala007@gmail.com>
2023-12-26 20:27:37 -0500

    added github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  target: 'experimental-serverless-trace',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `/${process.env.NEXT_PUBLIC_BASE_PATH}` : '',
 }
 
 module.exports = nextConfig
